69c69f02efb7cf2a9b3a2d1de5ab12a2cbf15e4e
Three Q <threeq1818@gmail.com>
2019-07-16 04:17:35 +0800

    create the Services model

---
+++ models/services.js
@@ -0,0 +1,26 @@
+// services.js
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+ServiceSchema = new Schema({
+    specialist_email: {
+        type: String,
+        required: true
+    },
+    service_type: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    hourly_rate: {
+        type: Number,
+        required: true
+    },
+    prefered_hour: {
+        type: String,
+        required: true
+    }
+});
