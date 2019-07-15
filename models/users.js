1b1784928ab207a933ba7f73ac0cdaaaf0e73c9d
Three Q <threeq1818@gmail.com>
2019-07-16 04:17:18 +0800

    create the Users model

---
+++ models/users.js
@@ -0,0 +1,30 @@
+// models/user.js
+
+const mongoose = requrire("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    account_type: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const User = mongoose.model('users', UserSchema);
+
+module.exports = User;
