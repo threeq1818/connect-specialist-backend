66899202462f8019d31f9712e809913e10e06a27
Three Q <threeq1818@gmail.com>
2019-07-17 00:01:58 +0800

    new methods: 	fetch all services 	fetch a service with _id 	fetch a services with specialist_id

--- routes/services.js
+++ routes/services.js
@@ -4,6 +4,8 @@ const express = require('express');
 const router = express.Router();
 const Service = require('../models/services');
 
+//  from specialist page
+//  insert a new service
 router.post('/', function (req, res) {
     const { error, isValid } = validateServiceInput(req.body);
 
@@ -23,3 +25,50 @@ router.post('/', function (req, res) {
             res.json(service);
         });
 });
+
+// from home page without authentication
+// fetch all services
+router.get('/fetchAll', function (req, res) {
+    Service.find({})
+        .then(services => {
+            res.json(services);
+        })
+        .catch(err => {
+            res.status(400).json(err);  //?????
+        })
+});
+
+// without authentication home page
+// fetch a service with _id
+router.get('/:id', function (req, res) {
+    const id = req.params.id;
+
+    Service.findById(id)
+        .then(service => {
+            res.json(service);
+        })
+        .catch(err => {
+            res.status(400).json(err);  //?????
+        });
+});
+
+// specialist page
+// fetch all services offered by the selected specialist
+router.get('/specialist/:id', function (req, res) {
+    const id = req.params.id;
+
+    Service.find({ specialist_id: id })
+        .then(services => {
+            res.json(services);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+// edit service inner page fo specialist page
+// update the service record with new data
+router.update('/specialist/:id', function (req, res) {
+    const id = req.params.id;
+
+})
