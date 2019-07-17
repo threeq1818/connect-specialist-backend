ec1fc30dd248bf1033819c67b73eff124d9c8024
Three Q <threeq1818@gmail.com>
2019-07-18 00:20:02 +0800

    correct the response json prototype added routes: 	get('/specialist/requestedProjs/:id') 	get('/customer/finishedProjs/:id')

--- routes/projects.js
+++ routes/projects.js
@@ -32,25 +32,96 @@ router.post('/', function (req, res) {
         })
 });
 
+
 // specialist page
 // fetch the finished projects by a specialist with id
 router.get('/specialist/finishedProjs/:id', async function (req, res) {
     const speialist_id = req.params.id;
-    // const user = await User.findById(speialist_id);
-    // const services = await Service.find({ specialist_id: speialist_id });
+    const user = await User.findById(speialist_id);
+    const services = await Service.find({ specialist_id: speialist_id });
 
-    const sidArray = services.map(a => a.service_id);
-    Project.find({ service_id: { in: sidArray } })
+    const sidArray = services.map(a => a._id);
+
+    let newProjectProtoType = [];
+    Project.find({ service_id: { $in: sidArray }, status: "finish" })
         .then(projects => {
             projects.forEach(element => {
-                element.specialist_email = user.email;
+                let newObj = element.toObject();
+                newObj.specialist_email = user.email;
                 let service = services.find(x => x._id == element.service_id);
-                element.service_type = service.service_type;
-                element.description = service.description;
-                element.hourly_rate = service.hourly_rate;
-                element.preferred_hour = service.preferred_hour;
+                newObj.service_type = service.service_type;
+                newObj.description = service.description;
+                newObj.hourly_rate = service.hourly_rate;
+                newObj.preferred_hour = service.preferred_hour;
+                newProjectProtoType.push(newObj);
             });
-            res.json(projects);
+            res.json(newProjectProtoType);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        })
+});
+
+// specialist page
+// fetch the requested projects by a specialist with id
+router.get('/specialist/requestedProjs/:id', async function (req, res) {
+    const speialist_id = req.params.id;
+    const user = await User.findById(speialist_id);
+    // console.log(user);
+    const services = await Service.find({ specialist_id: speialist_id });
+    // console.log(services);
+
+    const sidArray = services.map(a => a._id);
+    // console.log(sidArray);
+    let newProjectProtoType = [];
+    Project.find({ service_id: { $in: sidArray }, status: "request" })
+        .then(projects => {
+            projects.forEach((element, i) => {
+                let newObj = element.toObject();
+                newObj.specialist_email = user.email;
+                let service = services.find(x => x._id == element.service_id);
+                newObj.service_type = service.service_type;
+                newObj.description = service.description;
+                newObj.hourly_rate = service.hourly_rate;
+                newObj.preferred_hour = service.preferred_hour;
+                newProjectProtoType.push(newObj);
+            });
+            res.json(newProjectProtoType);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        })
+});
+
+// customer page
+// fetch the finnished projects of the selected customer by id
+router.get('/customer/finishedProjs/:id', async function (req, res) {
+    const customer_id = req.params.id;
+    const user = await User.findById(customer_id);
+    // console.log(user);
+    const services = await Service.find();
+    // console.log(services);
+
+    const sidArray = services.map(a => a._id);
+    // console.log(sidArray);
+
+    let newProjectProtoType = [];
+    Project.find({ customer_id: customer_id, status: "finish" })
+        .then(projects => {
+            projects.forEach(element => {
+                let newObj = element.toObject();
+                newObj.specialist_email = user.email;
+                let service = services.find(x => x._id == element.service_id);
+                newObj.service_type = service.service_type;
+                newObj.description = service.description;
+                newObj.hourly_rate = service.hourly_rate;
+                newObj.preferred_hour = service.preferred_hour;
+                newProjectProtoType.push(newObj);
+            });
+            res.json(newProjectProtoType);
+        })
+        .catch(err => {
+            res.status(400).json(err);
         })
 });
 
